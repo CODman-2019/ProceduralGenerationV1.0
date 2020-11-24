ee4cdb24707e9c6d072ab01b7a60df5eec5a44f6
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2020-11-24 15:56:29 -0400

    complete maze generator

---
+++ PrceduralGeneration_No/Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[Flags] // used to determine if a wall is 
+public enum WallState
+{
+    //0000 ->
+    //0001    0010       0100    1000
+    LEFT = 1, RIGHT = 2, UP = 4, DOWN= 8,
+    //1000 0000
+    VISITED = 128,
+
+    //player position
+    PLAYER = 16, //10000
+    //teleport position
+    TELEPORTER = 32
+}
+
+public struct Position
+{
+    public int x;
+    public int y;
+}
+
+public struct Neighbour
+{
+    public Position Position;
+    public WallState SharedWall;
+}
+
+
+public static class MazeGenerator 
+{
+
+    private static WallState GetOppositeWall(WallState wall)
+    {
+        switch (wall)
+        {
+            case WallState.RIGHT: return WallState.LEFT;
+            case WallState.LEFT: return WallState.RIGHT;
+            case WallState.UP: return WallState.DOWN;
+            case WallState.DOWN: return WallState.UP;
+            default: return WallState.VISITED;
+        }
+
+    }
+
+    //build maze itself
+    private static WallState[,] ApplyRecursiveBacktracker(WallState[,] maze, int width, int height, bool playerPlacement)
+    {
+        var rng = new System.Random(/*SEED*/);
+
+        bool teleportPlaced = false;
+        //create a stack variable
+        var positionStack = new Stack<Position>();
+
+
+        //pic a random point in the maze
+        var position = new Position { x = rng.Next(0, width), y = rng.Next(0, height)};
+
+        //set it spot as a visited state
+        maze[position.x, position.y] |= WallState.VISITED;
+
+        //add player state if the player placement is true;
+        if (playerPlacement)
+        {
+            maze[position.x, position.y] |= WallState.PLAYER;
+        }
+
+        positionStack.Push(position);
+
+        //go through the maze 
+        while(positionStack.Count > 0)
+        {
+            //remove the current position from the stack
+            var current = positionStack.Pop();
+            // get a list of neighbours
+            var neighbours = GetUnvisitedNeighbours(current, maze, width, height);
+
+
+            if(neighbours.Count > 0)
+            {
+                positionStack.Push(current);
+
+                var randIndex = rng.Next(0, neighbours.Count);
+                var randomNeighbour = neighbours[randIndex];
+
+                //get the neighbours position and remove the shared wall from both sides
+                var nPosition = randomNeighbour.Position;
+                maze[current.x, current.y] &= ~randomNeighbour.SharedWall; // remove the shared wall
+                maze[nPosition.x, nPosition.y] &= ~GetOppositeWall(randomNeighbour.SharedWall); // remove neighbours shared wall
+
+                //mark space as visited
+                maze[nPosition.x, nPosition.y] |= WallState.VISITED;
+
+                //push into the stack
+                positionStack.Push(nPosition);
+            }
+            else
+            {
+                if (!teleportPlaced)
+                {
+                    maze[current.x, current.y] |= WallState.TELEPORTER;
+                    teleportPlaced = false;
+                }
+                
+            }
+        }
+
+
+        return maze;
+    }
+
+    //return a list of nieighbours that were not visited
+    private static List<Neighbour> GetUnvisitedNeighbours(Position p, WallState[,] maze, int width, int height)
+    {
+        var list = new List<Neighbour>();
+
+        if(p.x > 0) // left
+        {
+            //if the node is not visited
+            if(!maze[p.x - 1, p.y].HasFlag(WallState.VISITED))
+            {
+                //add neighbour to list
+                list.Add(new Neighbour
+                {
+                    Position = new Position
+                    {
+                        x = p.x - 1,
+                        y = p.y
+                    },
+                    SharedWall = WallState.LEFT
+                });
+            }
+        }
+
+        if (p.y > 0) // Bottom
+        {
+            if (!maze[p.x, p.y - 1].HasFlag(WallState.VISITED))
+            {
+                list.Add(new Neighbour
+                {
+                    Position = new Position
+                    {
+                        x = p.x,
+                        y = p.y - 1
+                    },
+                    SharedWall = WallState.DOWN
+                });
+            }
+        }
+
+        if (p.y < height - 1) // top
+        {
+            if (!maze[p.x, p.y + 1].HasFlag(WallState.VISITED))
+            {
+                list.Add(new Neighbour
+                {
+                    Position = new Position
+                    {
+                        x = p.x,
+                        y = p.y + 1
+                    },
+                    SharedWall = WallState.UP
+                });
+            }
+        }
+
+        if (p.x < width - 1) // right
+        {
+            if (!maze[p.x + 1, p.y].HasFlag(WallState.VISITED))
+            {
+                list.Add(new Neighbour
+                {
+                    Position = new Position
+                    {
+                        x = p.x + 1,
+                        y = p.y
+                    },
+                    SharedWall = WallState.RIGHT
+                });
+            }
+        }
+        return list;
+    }
+
+    //create empty grid
+    public static WallState[,] Generate(int width, int height, bool playerPlacement)
+    {
+        WallState[,] maze = new WallState[width, height];
+        WallState startState = WallState.RIGHT | WallState.LEFT | WallState.UP | WallState.DOWN;
+
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                maze[x, y] = startState; // 1111
+
+            }
+        }
+
+
+
+        return ApplyRecursiveBacktracker(maze, width, height, playerPlacement);
+
+    }
+}
---
+++ PrceduralGeneration_No/Assets/Scripts/MazeRenderer.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeRenderer : MonoBehaviour
+{
+    [SerializeField]
+    private Transform wallPrefab = null;
+
+    [SerializeField]
+    private Transform floorObject = null;
+
+    public Transform FPSController;
+    public Transform teleporter;
+
+    [Range(1, 50)]
+    public int width;
+    [Range(1, 50)]
+    public int height;
+
+    [Range(1, 50)]
+    public int levels;
+
+    [SerializeField]
+    private float size = 1f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //WallState[] mazes = new WallState[levels];
+        var mazes = new List<WallState[,]>();
+
+        //for (int x = 0; x < levels; x++)
+        //{
+        //    //var maze = 
+        //    mazes[x] = MazeGenerator.Generate(width, height, true);;
+        //}
+        var maze = MazeGenerator.Generate(width, height, true);
+        Draw(maze);
+
+        //var rnd = new System.Random();
+
+    }
+
+    //builds maze
+    private void Draw(WallState[,] maze)
+    {
+
+        //creating the floor
+        var floor = Instantiate(floorObject);
+        floor.localScale = new Vector3(width, 0.1f, height);
+
+        //go through the width and height
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j< height; j++)
+            {
+                //create a cell
+                var cell = maze[i, j];
+                //adjust the position
+                var position = new Vector3(-width / 2 + i, 0, -height / 2 + j);
+
+                //creating and instantiating walls based on which wall is made
+                if (cell.HasFlag(WallState.UP))
+                {
+
+                    var topWall = Instantiate(wallPrefab, transform);
+                    topWall.position = position + new Vector3(0, 0, size/2);
+                    topWall.localScale = new Vector3(size, topWall.localScale.y, topWall.localScale.z);
+                }
+
+                if (cell.HasFlag(WallState.LEFT))
+                {
+                    var leftWall = Instantiate(wallPrefab, transform);
+                    leftWall.position = position + new Vector3(-size / 2, 0, 0);
+                    leftWall.localScale = new Vector3(size, leftWall.localScale.y, leftWall.localScale.z);
+                    leftWall.eulerAngles = new Vector3(0, 90, 0);
+                }
+
+                //if the loop reaches the las collum it builds the right wall
+                if(i == width - 1)
+                {
+                    if (cell.HasFlag(WallState.RIGHT))
+                    {
+                        var rightWall = Instantiate(wallPrefab, transform);
+                        rightWall.position = position + new Vector3(size / 2, 0, 0);
+                        rightWall.localScale = new Vector3(size, rightWall.localScale.y, rightWall.localScale.z);
+                        rightWall.eulerAngles = new Vector3(0, 90, 0);
+                    }
+                }
+
+                //if the loop is on the first collom, it builds the bottom wall
+                if (j == 0)
+                {
+                    if (cell.HasFlag(WallState.DOWN))
+                    {
+                        var bottomWall = Instantiate(wallPrefab, transform);
+                        bottomWall.position = position + new Vector3(0, 0, -size / 2);
+                        bottomWall.localScale = new Vector3(size, bottomWall.localScale.y, bottomWall.localScale.z);
+                    }
+                }
+
+                if (cell.HasFlag(WallState.PLAYER))
+                {
+                    var player = Instantiate(FPSController, transform);
+                    //player.position = position;
+                }
+
+                //if (cell.HasFlag(WallState.TELEPORTER))
+                //{
+                //    var teleport = Instantiate(teleporter, transform) as Transform;
+                //}
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    var maze = MazeGenerator.Generate(width, height, true);
+        //    Draw(maze);
+        //}   
+    }
+}
+
+
+
+
+
+
+
+
+
+// as Transform
